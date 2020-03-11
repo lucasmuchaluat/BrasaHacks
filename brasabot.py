1da15fb72e5d010144269f367ce3debfb8c74a77
ThiagoSTeixeira <tteixeira.st@gmail.com>
2020-03-10 22:00:42 -0300

    add bot

---
+++ brasabot.py
@@ -0,0 +1,43 @@
+"""
+Tips and notes:
+    -As soon as you add new handlers to dispatcher, they are in effect.
+    -The Filters class contains a number of functions that filter incoming messages for text, images, status updates and more.
+     Any message that returns True for at least one of the filters passed to MessageHandler will be accepted. You can also write your own filters if you want.
+
+Commands and requests:
+    /saldo - Devolve quanto dinheiro o usuario tem na conta corrente
+    /extrato - Pergunta qual extrato vc quer receber: diario, semanal e mensal
+    /minhaconta - Devolve uma mensagem para ser compartilhada com outros usuarios para realizar a transferenecia
+    /transferencia - Recebe a mensagem gerada em /minhaconta por outro usuario e pergunta o valor a ser transferido
+    /calcula - Recebe o valor a vista e o preco da prestacao 
+"""
+
+#Headers and bot initiation
+from telegram.ext import Updater
+updater = Updater(token = '1105160055:AAGkIIuk_M4B3r_uwumehJwJ8ZLwQDqylqo', use_context = True)
+dispatcher = updater.dispatcher
+
+#Sets up the logging module
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+#Bot functions
+def start(update, context):
+    reply_keyboard = [['Saldo', 'Extrato', 'Transferencia', 'Calcula']]
+    context.bot.send_message(chat_id=update.effective_chat.id, text= "Oi! Eu sou seu assistente pessoal da NuBank, o que quer fazer?")
+    
+def echo(update, context):
+    """Echoes sent message, testing usage only
+    """
+    context.bot.send_message(chat_id=update.effective_chat.id, text = update.message.text)
+#Command Handler
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+from telegram.ext import MessageHandler, Filters
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
+
+#Start the bot
+updater.start_polling()
---
+++ users.py
@@ -0,0 +1,40 @@
+class Usuario:
+    def __init__(self, accountNum, name, balance, extractM, extractW, extractD):
+        self.accountNum = accountNum
+        self.name = name
+        self.balance = balance
+        self.extractM = extractM
+        self.extractW = extractW
+        self.extractD = extractD
+
+extractM = "\
+        05/03/2020    Jogos       R$ 250.00\n\
+        16/03/2020    Cantina     R$ 10.00\n \
+        17/03/2020    Papelaria   R$ 15.00\n\
+        20/03/2020    Eventos     R$ 300.00\n\
+        25/03/2020    Gasolina    R$ 170.00\n\
+        27/03/2020    Mercado     R$ 150.00\n\
+        Total:                    R$ 895.00\n\
+        "
+extractW = "\
+        20/03/2020    Eventos     R$ 300.00\n\
+        25/03/2020    Gasolina    R$ 170.00\n\
+        27/03/2020    Mercado     R$ 150.00\n\
+        27/03/2020    Lojinha     R$ 7.00\n\
+        Total:                    R$ 627.00\
+        "
+extractD = "\
+        27/03/2020    Mercado     R$ 150.00\n\
+        27/03/2020    Lojinha     R$ 7.00\n\
+        Total:                    R$ 157.00\n\
+        "
+
+usuario1 = Usuario(999, "Cleber", 1400, extractM, extractW, extractD)
+usuario2 = Usuario(888, "Joana", 7000, extractM, extractW, extractD)
+usuario3 = Usuario(777, "Lucas", -200 , extractM, extractW, extractD)
+
+
+
+
+
+
